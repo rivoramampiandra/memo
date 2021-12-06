c877f8cb3949cda36e77f8ea272da78ea6c46640
zoniaina <fzoniaina@gmail.com>
2021-12-06 18:44:08 +0300

    condition placeholder

---
+++ src/containers/Conditions/Conditions.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Conditions = () => {
+  return (
+    <View>
+      <Text>Condition général d'utilisation</Text>
+    </View>
+  );
+};
+
+export default Conditions;
