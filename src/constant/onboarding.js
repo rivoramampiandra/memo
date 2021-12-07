10b19416f20c226310ceaa3ee166f834c586ce98
zoniaina <fzoniaina@gmail.com>
2021-12-07 15:30:21 +0300

    onboarding data

---
+++ src/constant/onboarding.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    title: 'Ajouter vos documents',
+    description:
+      "Prenez en photo l’ensemble de vos factures d’entretiens pour commencer l'accompagnement en un clic sur l'îcone en haut à droite",
+    image: require('../assets/images/onboarding-1.png'),
+  },
+  {
+    id: 2,
+    title: 'Vos données sont protégées',
+    description:
+      'Toutes vos données sont annonymisées afin de ne garder que l’essentiel.',
+    image: require('../assets/images/onboarding-2.png'),
+  },
+  {
+    id: 3,
+    title: 'Diagnostique complet',
+    description:
+      'Visualiser en 1 clin d’oeil ce qui est à réaliser et consulter le carnet de santé de votre voiture. ',
+    image: require('../assets/images/onboarding-3.png'),
+  },
+  {
+    id: 4,
+    title: 'Trouvez le bon réparateur ',
+    description:
+      'Prenez rendez-vous en quelques clics parmi un panel de garages et bénéficiez de remises exceptionnelles.',
+    image: require('../assets/images/onboarding-4.png'),
+  },
+  {
+    id: 5,
+    title: 'Roulez l’esprit tranquille ',
+    description:
+      'Faites nous confiance, on vous  alertera lorsque quelque chose sera à faire.',
+    image: require('../assets/images/onboarding-5.png'),
+  },
+];
