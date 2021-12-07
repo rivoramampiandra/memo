99bc9b42188a8bc1041f0d301a5a87e19b733e4b
zoniaina <fzoniaina@gmail.com>
2021-12-07 15:31:06 +0300

    Home onboarding condition

---
+++ src/containers/Home/Home.style.ts
@@ -0,0 +1,10 @@
+import {StatusBar, StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingTop: StatusBar.currentHeight,
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import Onboarding from '../../components/Onboarding/Onboarding';
+import {styles} from './Home.style';
 
 const Home = () => {
+  const [firstLogin, setFirstLogin] = useState(false);
+  const [done, setDone] = useState(false);
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {firstLogin && done ? (
+        <ScrollView>
+          <Text>Home</Text>
+        </ScrollView>
+      ) : (
+        <Onboarding setDone={setDone} setFirstLogin={setFirstLogin} />
+      )}
+    </SafeAreaView>
   );
 };
 
