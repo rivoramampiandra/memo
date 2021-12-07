6e16f6f0c0d25433d66f4729bdea1eacb826150f
zoniaina <fzoniaina@gmail.com>
2021-12-07 15:30:50 +0300

    ui kitten provider setting

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,11 +1,16 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import SignIn from './Auth/SignIn/SignIn';
 import SignUp from './Auth/SignUp/SignUp';
 import Conditions from './Conditions/Conditions';
 import Home from './Home/Home';
+import * as eva from '@eva-design/eva';
+import {default as defaultTheme} from '../theme/default-theme.json';
+import {default as mapping} from '../../mapping.json';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
 
 const {Screen, Navigator} = createNativeStackNavigator();
 
@@ -15,14 +20,24 @@ const App = () => {
   }, []);
 
   return (
-    <NavigationContainer>
-      <Navigator initialRouteName="SignIn" screenOptions={{headerShown: false}}>
-        <Screen name="SignIn" component={SignIn} />
-        <Screen name="SignUp" component={SignUp} />
-        <Screen name="Home" component={Home} />
-        <Screen name="Conditions" component={Conditions} />
-      </Navigator>
-    </NavigationContainer>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider
+        {...eva}
+        theme={{...eva.light, ...defaultTheme}}
+        customMapping={mapping as any}>
+        <NavigationContainer>
+          <Navigator
+            initialRouteName="SignIn"
+            screenOptions={{headerShown: false}}>
+            <Screen name="SignIn" component={SignIn} />
+            <Screen name="SignUp" component={SignUp} />
+            <Screen name="Home" component={Home} />
+            <Screen name="Conditions" component={Conditions} />
+          </Navigator>
+        </NavigationContainer>
+      </ApplicationProvider>
+    </>
   );
 };
 
