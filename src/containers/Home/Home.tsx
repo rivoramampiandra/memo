69e5e87bba591c8fc9b836178592aa6892a03964
zoniaina <fzoniaina@gmail.com>
2021-12-16 13:09:15 +0300

    update home navigation

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,18 +1,27 @@
+import {DrawerActions} from '@react-navigation/native';
 import {Text} from '@ui-kitten/components';
 import React, {useState} from 'react';
-import {SafeAreaView, ScrollView} from 'react-native';
+import {SafeAreaView, ScrollView, TouchableOpacity} from 'react-native';
 import Onboarding from '../../components/Onboarding/Onboarding';
 import {styles} from './Home.style';
 
-const Home = () => {
+const Home = ({navigation}: any) => {
   const [firstLogin, setFirstLogin] = useState(false);
   const [done, setDone] = useState(false);
+
+  const handleOpenDrawer = () => {
+    navigation.openDrawer();
+    // navigation.dispatch(DrawerActions.openDrawer());
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       {!firstLogin && done ? (
         <ScrollView>
           <Text>Home</Text>
+          <TouchableOpacity onPress={handleOpenDrawer}>
+            <Text>Open</Text>
+          </TouchableOpacity>
         </ScrollView>
       ) : (
         <Onboarding setDone={setDone} setFirstLogin={setFirstLogin} />
