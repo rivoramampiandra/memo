df0929047ba4b4fab8ffb61d34f58da56a661820
zoniaina <fzoniaina@gmail.com>
2021-12-16 13:09:27 +0300

    update app navigation

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,18 +1,13 @@
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
-import SignIn from './Auth/SignIn/SignIn';
-import SignUp from './Auth/SignUp/SignUp';
 import * as eva from '@eva-design/eva';
 import {default as defaultTheme} from '../theme/default-theme.json';
 import {default as mapping} from '../../mapping.json';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
-import HomeNavigation from '../navigation/HomeNavigation';
-import Home from './Home/Home';
-
-const {Screen, Navigator} = createNativeStackNavigator();
+import 'react-native-gesture-handler';
+import AuthNavigation from '../navigation/AuthNavigation';
 
 const App = () => {
   useEffect(() => {
@@ -27,13 +22,7 @@ const App = () => {
         theme={{...eva.light, ...defaultTheme}}
         customMapping={mapping as any}>
         <NavigationContainer>
-          <Navigator
-            initialRouteName="SignIn"
-            screenOptions={{headerShown: false}}>
-            <Screen name="SignIn" component={SignIn} />
-            <Screen name="SignUp" component={SignUp} />
-            <Screen name="Home" component={Home} />
-          </Navigator>
+          <AuthNavigation />
         </NavigationContainer>
       </ApplicationProvider>
     </>
