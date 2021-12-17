edcf4ff4d17617e2ec0897fa8e116038822bd1f5
zoniaina <fzoniaina@gmail.com>
2021-12-17 16:31:51 +0300

    sidemenu update

--- src/components/Sidemenu/sidemenu.style.ts
+++ src/components/Sidemenu/sidemenu.style.ts
@@ -5,12 +5,27 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'space-between',
     borderBottomRightRadius: 15,
+    padding: 25,
+  },
+  item: {
+    paddingVertical: 18,
+    paddingLeft: 12,
+  },
+  textItem: {
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+  activeItem: {
+    borderLeftWidth: 4,
+    borderLeftStyle: 'solid',
+    borderLeftColor: '#30AAC7',
   },
   miniDivider: {
-    width: '40%',
+    width: '25%',
     borderBottomWidth: 1,
     borderBottomColor: '#24272D',
     borderBottomStyle: 'solid',
+    marginTop: 8,
   },
   activeListe: {
     borderLeftColor: 'red',
@@ -23,5 +38,11 @@ export const styles = StyleSheet.create({
   },
   logo: {
     height: 50,
+  },
+  footer: {
+    flex: 0.2,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
