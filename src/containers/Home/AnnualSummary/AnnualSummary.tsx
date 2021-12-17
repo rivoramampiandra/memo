1ea2f3886772a8c2713598b2f241ed24d6a52c4d
zoniaina <fzoniaina@gmail.com>
2021-12-17 17:31:21 +0300

    wip : home

---
+++ src/containers/Home/AnnualSummary/AnnualSummary.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const AnnualSummary = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default AnnualSummary;
