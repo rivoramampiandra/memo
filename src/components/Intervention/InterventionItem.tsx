435cd90171abfc645578287ec2aea1e323842b9f
zoniaina <fzoniaina@gmail.com>
2021-12-23 14:24:34 +0300

    interventio item wip

--- src/components/Intervention/InterventionItem.tsx
+++ src/components/Intervention/InterventionItem.tsx
@@ -1,10 +1,52 @@
+import {Icon, Text} from '@ui-kitten/components';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import {styles} from './InterventionItem.style';
 
-const InterventionItem = () => {
+interface IIntervention {
+  item: {
+    title: string;
+    subtitle: string;
+    image: string;
+    distance: string;
+    type: string;
+  };
+  rest?: any;
+}
+
+const InterventionItem = (props: IIntervention) => {
+  const {title, subtitle, image, type, distance} = props.item;
+
+  const getStatusColor = (type: string) => {
+    const color: any = {
+      default: 'primary',
+      warning: 'warning',
+      critical: 'danger',
+    };
+    return type ? color[type] : color['default'];
+  };
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Icon name="shield-off-outline" fill="#000" style={styles.itemImage} />
+      </View>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subtitle}>{subtitle}</Text>
+      </View>
+      <View style={styles.distanceContainer}>
+        <Text status={getStatusColor(type)} style={styles.distance}>
+          {distance}
+        </Text>
+      </View>
+      <View style={styles.iconContainer}>
+        <Icon
+          name="chevron-right-outline"
+          fill="#000"
+          style={styles.iconChevron}
+        />
+      </View>
     </View>
   );
 };
