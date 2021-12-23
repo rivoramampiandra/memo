b6c6dd9969b3520b4546d86721a663d798aa5386
zoniaina <fzoniaina@gmail.com>
2021-12-23 14:25:05 +0300

    const mock - temp img

--- src/constant/image.ts
+++ src/constant/image.ts
@@ -1,3 +1,4 @@
 export default {
   LOGO: require('../assets/images/h-logo.png'),
+  TEMP_CHART: require('../assets/images/temp_chart.png'),
 };
---
+++ src/constant/intervention-mock.ts
@@ -0,0 +1,59 @@
+export const upcoming = [
+  {
+    image: '',
+    title: 'Batterie',
+    subtitle: "Contrôle d'usure / remplacement",
+    distance: 'dans 40 km',
+    type: 'default',
+  },
+  {
+    image: '',
+    title: 'Huile moteur',
+    subtitle: 'Remplacement',
+    distance: 'dans 40 km',
+    type: 'default',
+  },
+  {
+    image: '',
+    title: "Ligne d'échappement",
+    subtitle: "Contrôle d'usure / remplacement",
+    distance: 'dans 40 km',
+    type: 'default',
+  },
+  {
+    image: '',
+    title: 'Chassis',
+    subtitle: "Contrôle d'usure",
+    distance: 'dans 40 km',
+    type: 'default',
+  },
+];
+
+export const toReplace = [
+  {
+    image: '',
+    title: 'Disque de frein avant',
+    subtitle: "Contrôle d'usure / remplacement",
+    distance: 'dans 40 km',
+    type: 'critical',
+  },
+  {
+    image: '',
+    title: 'Liquide de refroidissement',
+    subtitle: 'Remplacement',
+    distance: 'dans 40 km',
+    type: 'warning',
+  },
+  {
+    image: '',
+    title: 'Aile avant droite',
+    subtitle: "Contrôle d'usure / remplacement",
+    distance: 'dans 40 km',
+    type: 'warning',
+  },
+];
+
+// export default {
+//   upcoming,
+//   toReplace,
+// };
