5d380158b953882c40ce49195cb796340c648caf
zoniaina <fzoniaina@gmail.com>
2021-12-23 14:25:13 +0300

    dashboard wip

--- src/containers/Home/Dashboard/Dashboard.tsx
+++ src/containers/Home/Dashboard/Dashboard.tsx
@@ -1,10 +1,45 @@
+import {Text} from '@ui-kitten/components';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  useWindowDimensions,
+  View,
+} from 'react-native';
+import {styles} from '../Home.style';
+import {upcoming, toReplace} from '../../../constant/intervention-mock';
+import {InterventionItem} from '../../../components/Intervention';
+import image from '../../../constant/image';
 
 const Dashboard = () => {
+  const {width, height} = useWindowDimensions();
   return (
-    <View>
-      <Text></Text>
+    <View style={{flex: 1}}>
+      <Text style={styles.containerTitle}>TABLEAU DE BORD</Text>
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <Image
+          source={image.TEMP_CHART}
+          style={{width: width, height: height * 0.4}}
+          resizeMode="contain"
+        />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View>
+          <Text>A remplacer</Text>
+          <FlatList
+            data={toReplace}
+            renderItem={({item}) => <InterventionItem item={item} />}
+          />
+        </View>
+        <View>
+          <Text>Intervention à venir</Text>
+          <FlatList
+            data={upcoming}
+            renderItem={({item}) => <InterventionItem item={item} />}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
