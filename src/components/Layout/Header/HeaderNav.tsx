fae2b997f76d6d975a27033ba418200b9fce50da
zoniaina <fzoniaina@gmail.com>
2021-12-23 14:24:42 +0300

    header nav wip

--- src/components/Layout/Header/HeaderNav.tsx
+++ src/components/Layout/Header/HeaderNav.tsx
@@ -1,21 +1,35 @@
-import {Icon} from '@ui-kitten/components';
+import {useDrawerStatus} from '@react-navigation/drawer';
+import {DrawerActions, useNavigation} from '@react-navigation/native';
+import {Icon, Text} from '@ui-kitten/components';
 import React from 'react';
-import {View, Image, Text} from 'react-native';
+import {View, Image, TouchableOpacity} from 'react-native';
 import image from '../../../constant/image';
 import {styles} from './Header.style';
 
-const HeaderNav = () => {
+const HeaderNav = (props: any) => {
+  const isDrawerOpen = useDrawerStatus() === 'open';
+  const navigation = useNavigation();
+
+  const handleOpenDrawer = () => {
+    navigation.dispatch(DrawerActions.toggleDrawer());
+  };
   return (
-    <View>
-      <View>
-        <Icon name="menu-outline" fill="#000" style={{width: 32, height: 32}} />
-      </View>
-      <Text>Ma peugeot 206</Text>
-      <Icon
-        name="file-text-outline"
-        fill="#000"
-        style={{width: 32, height: 32}}
-      />
+    <View style={styles.navContainer}>
+      <TouchableOpacity onPress={handleOpenDrawer}>
+        <Icon
+          name={!isDrawerOpen ? 'menu-outline' : 'close-outline'}
+          fill="#000"
+          style={{width: 32, height: 32}}
+        />
+      </TouchableOpacity>
+      <Text style={styles.navTitle}>Ma peugeot 206</Text>
+      <TouchableOpacity>
+        <Icon
+          name="file-text-outline"
+          fill="#000"
+          style={{width: 32, height: 32}}
+        />
+      </TouchableOpacity>
     </View>
   );
 };
