247ae735f558cd97bc54b7a0e013597e82718086
zoniaina <fzoniaina@gmail.com>
2022-01-03 23:28:51 +0300

    wrapper update

--- src/components/Layout/Wrapper.tsx
+++ src/components/Layout/Wrapper.tsx
@@ -1,8 +1,8 @@
 import React, {ReactElement} from 'react';
 import {SafeAreaView} from 'react-native';
 
-const Wrapper = (component: ReactElement) => {
-  return <SafeAreaView>{component}</SafeAreaView>;
+const Wrapper = ({children}: any) => {
+  return <SafeAreaView style={{flex: 1, padding: 20}}>{children}</SafeAreaView>;
 };
 
 export default Wrapper;
