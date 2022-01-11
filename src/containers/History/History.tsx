f83e8da6edcea940496de6dc2a7f48d1c699f729
zoniaina <fzoniaina@gmail.com>
2022-01-12 00:47:04 +0300

    history container

---
+++ src/containers/History/History.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {Indicator} from '../../components/Indicator';
+import Timeline from './Timeline/Timeline';
+
+const History = () => {
+  return (
+    <View>
+      <Text></Text>
+      <Indicator />
+      <Timeline />
+    </View>
+  );
+};
+
+export default History;
