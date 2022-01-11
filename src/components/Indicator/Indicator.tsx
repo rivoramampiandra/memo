19b07dd4a2807fd9b3a7338afed67dbe65ffcd7a
zoniaina <fzoniaina@gmail.com>
2022-01-12 00:42:02 +0300

    wip indicator

---
+++ src/components/Indicator/Indicator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Indicator = () => {
+  return (
+    <View>
+      <Text>Indicator here</Text>
+    </View>
+  );
+};
+
+export default Indicator;
