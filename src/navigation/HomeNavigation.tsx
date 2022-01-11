1a155caa3a44ebb3f710dd52d470b976d4c5d3dd
zoniaina <fzoniaina@gmail.com>
2022-01-12 00:44:05 +0300

    update navigation

--- src/navigation/HomeNavigation.tsx
+++ src/navigation/HomeNavigation.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import Home from '../containers/Home/Home';
 import Sidemenu from '../components/Sidemenu/Sidemenu';
-import {NavigationContainer} from '@react-navigation/native';
 import SignIn from '../containers/Auth/SignIn/SignIn';
 import AuthNavigation from './AuthNavigation';
 import {Kilometrage} from '../containers/Kilometrage';
+import {Intervention} from '../containers/Intervention';
+import {Vehicles} from '../containers/Vehicles';
+import {Setting} from '../containers/Setting';
+import SignUp from '../containers/Auth/SignUp/SignUp';
+import {History} from '../containers/History';
+import {PDFViewer} from '../containers/PDFViewer';
 
 const {Navigator, Screen} = createDrawerNavigator();
 
@@ -25,6 +30,12 @@ const HomeNavigation = () => {
       <Screen name="Home" component={Home} />
       <Screen name="Kilometrage" component={Kilometrage} />
       <Screen name="SignIn" component={SignIn} />
+      <Screen name="SignUp" component={SignUp} />
+      <Screen name="Intervention" component={Intervention} />
+      <Screen name="Vehicles" component={Vehicles} />
+      <Screen name="Setting" component={Setting} />
+      <Screen name="PDFViewer" component={PDFViewer} />
+      <Screen name="History" component={History} />
     </Navigator>
   );
 };
