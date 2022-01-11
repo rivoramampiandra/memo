9e8ec1f58b0da8e47329de0e21689f65b7fd9f91
zoniaina <fzoniaina@gmail.com>
2022-01-12 00:46:31 +0300

    kilometrage container

--- src/containers/Kilometrage/Kilometrage.tsx
+++ src/containers/Kilometrage/Kilometrage.tsx
@@ -1,45 +1,146 @@
 import {Button, Icon, Text} from '@ui-kitten/components';
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {View, TouchableOpacity, useWindowDimensions} from 'react-native';
 import Wrapper from '../../components/Layout/Wrapper';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+//@ts-ignore:next-line
+import Picker from 'react-native-roll-picker';
+import {styles} from './Kilometrage.style';
 
 const Kilometrage = (props: any) => {
   const {navigation} = props;
   const {height} = useWindowDimensions();
+  const [distance, setDistance] = useState(String(40).padStart(6));
+  const defaultRange = {
+    rowIndex0: '0',
+    rowIndex1: '0',
+    rowIndex2: '0',
+    rowIndex3: '0',
+    rowIndex4: '0',
+    rowIndex5: '0',
+  };
+  const [range, setRange] = useState(defaultRange);
+  const _Picker0: any = useRef();
+  const _Picker1: any = useRef();
+  const _Picker2: any = useRef();
+  const _Picker3: any = useRef();
+  const _Picker4: any = useRef();
+  const _Picker5: any = useRef();
+
+  const takePicture = () => {
+    launchCamera({mediaType: 'photo'}, res => {
+      console.log(res);
+    });
+  };
+
+  const PICKER_DATA = [
+    {index: '0'},
+    {index: '1'},
+    {index: '2'},
+    {index: '3'},
+    {index: '4'},
+    {index: '5'},
+    {index: '6'},
+    {index: '7'},
+    {index: '8'},
+    {index: '9'},
+  ];
+
+  const updateDistance = () => {
+    const dataRange = Object.values(range).join('');
+    setDistance(dataRange);
+  };
+
+  const initDistance = () => {
+    const dataDistance = '320000';
+    const rangePicker = dataDistance.split('');
+    setRange({
+      rowIndex0: rangePicker[0],
+      rowIndex1: rangePicker[1],
+      rowIndex2: rangePicker[2],
+      rowIndex3: rangePicker[3],
+      rowIndex4: rangePicker[4],
+      rowIndex5: rangePicker[5],
+    });
+    setDistance(dataDistance);
+  };
+
+  useEffect(() => {
+    if (range) {
+      updateDistance();
+    }
+  }, [range]);
+
+  useEffect(() => {}, [_Picker0]);
+
   return (
     <Wrapper>
-      <View
-        style={{
-          flexDirection: 'row',
-          paddingVertical: 5,
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}>
+      <View style={styles.container}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <Icon
             name="chevron-left-outline"
             fill="#000"
-            style={{width: 32, height: 32}}
+            style={styles.defaultIcon}
           />
         </TouchableOpacity>
         <View>
-          <Text>Kilometrage</Text>
+          <Text category={'h6'}>Kilometrage global</Text>
         </View>
-        <TouchableOpacity>
-          <Icon
-            name="camera-outline"
-            fill="#000"
-            style={{width: 32, height: 32}}
-          />
+        <TouchableOpacity onPress={takePicture}>
+          <Icon name="camera-outline" fill="#000" style={styles.defaultIcon} />
         </TouchableOpacity>
       </View>
-      <View
-        style={{
-          height: height * 0.4,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text>320 000 km</Text>
+      {/* <Text>{distance}</Text> */}
+      <View style={[styles.pickerContainer, {height: height * 0.4}]}>
+        <Picker
+          data={PICKER_DATA}
+          ref={_Picker0}
+          name="index"
+          onRowChange={(index: any) => {
+            setRange({...range, rowIndex0: index});
+          }}
+        />
+        <Picker
+          data={PICKER_DATA}
+          ref={_Picker1}
+          name="index"
+          onRowChange={(index: any) => {
+            setRange({...range, rowIndex1: index});
+          }}
+        />
+        <Picker
+          data={PICKER_DATA}
+          ref={_Picker2}
+          name="index"
+          onRowChange={(index: any) => {
+            setRange({...range, rowIndex2: index});
+          }}
+        />
+        <Picker
+          data={PICKER_DATA}
+          ref={_Picker3}
+          name="index"
+          onRowChange={(index: any) => {
+            setRange({...range, rowIndex3: index});
+          }}
+        />
+        <Picker
+          data={PICKER_DATA}
+          ref={_Picker4}
+          name="index"
+          onRowChange={(index: any) => {
+            setRange({...range, rowIndex4: index});
+          }}
+        />
+        <Picker
+          data={PICKER_DATA}
+          ref={_Picker5}
+          name="index"
+          onRowChange={(index: any) => {
+            setRange({...range, rowIndex5: index});
+          }}
+        />
+        <Text>Km</Text>
       </View>
       <Button>Enregistrer</Button>
     </Wrapper>
