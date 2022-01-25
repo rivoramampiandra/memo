ac2deb6fd5799aeadcc19a7c55037f0210381265
zoniaina <fzoniaina@gmail.com>
2022-01-25 10:16:33 +0300

    wip: add car

---
+++ src/components/AddCarForm/AddCarForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from './AddCarForm.style';
+
+const AddCarForm = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default AddCarForm;
