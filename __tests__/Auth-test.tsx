b8b213bbb4114d3afc83e8ae1039a18ae052fecb
zoniaina <fzoniaina@gmail.com>
2022-02-09 10:31:10 +0300

    wip test

---
+++ __tests__/Auth-test.tsx
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+
+describe('authentication test', () => {
+  it('should return token', () => undefined);
+  it('return false if user logged out', () => undefined);
+  it('return true if user logged in', () => undefined);
+});
