5c53c1c527a7e7974a8a7b6440731f752c489660
Zoniaina <fzoniaina@gmail.com>
2022-02-24 09:25:08 +0300

    axios lib setupe

--- src/lib/axios.lib.ts
+++ src/lib/axios.lib.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const BASE_URL = 'https://app.memoracar.com';
 const PORT = 16000;
+const OCR_BASE_URL = 'https://ocr.memoracar.com';
+const OCR_PORT = '20000';
+const AUTHKEY = '8bf54562-3000-11eb-adc1-0242ac120002';
 
 const axiosClient = axios.create({
   baseURL: `${BASE_URL}:${PORT}/`,
@@ -9,6 +12,16 @@ const axiosClient = axios.create({
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
+  },
+});
+
+export const axiosOCRClient = axios.create({
+  baseURL: `${OCR_BASE_URL}:${OCR_PORT}/`,
+  timeout: 3000,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Authorization: AUTHKEY,
   },
 });
 
