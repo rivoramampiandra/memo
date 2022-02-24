b0614be73f2a2c01b9b602eb1b18e12dff4fa251
Zoniaina <fzoniaina@gmail.com>
2022-02-24 09:25:50 +0300

    update navigation

--- src/navigation/AuthNavigation.tsx
+++ src/navigation/AuthNavigation.tsx
@@ -1,34 +1,15 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React, {useContext, useEffect} from 'react';
+import React from 'react';
 import SignIn from '../containers/Auth/SignIn/SignIn';
 import SignUp from '../containers/Auth/SignUp/SignUp';
-import Home from '../containers/Home/Home';
-import {AuthContext} from '../providers/AuthProvider';
-import {AsyncStorageUtils} from '../utils/asyncStorageUtils';
 
 const {Screen, Navigator} = createNativeStackNavigator();
 
 const AuthNavigation = () => {
-  const {token, setToken} = useContext(AuthContext);
-
-  useEffect(() => {
-    AsyncStorageUtils.getToken().then(res => {
-      setToken(res);
-    });
-  }, []);
-
   return (
     <Navigator initialRouteName="SignIn" screenOptions={{headerShown: false}}>
-      {token ? (
-        <>
-          <Screen name="SignIn" component={SignIn} />
-          <Screen name="SignUp" component={SignUp} />
-        </>
-      ) : (
-        <>
-          <Screen name="Home" component={Home} />
-        </>
-      )}
+      <Screen name="SignIn" component={SignIn} />
+      <Screen name="SignUp" component={SignUp} />
     </Navigator>
   );
 };
--- src/navigation/HomeNavigation.tsx
+++ src/navigation/HomeNavigation.tsx
@@ -1,32 +1,19 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import Home from '../containers/Home/Home';
 import Sidemenu from '../components/Sidemenu/Sidemenu';
-import SignIn from '../containers/Auth/SignIn/SignIn';
-import AuthNavigation from './AuthNavigation';
 import {Kilometrage} from '../containers/Kilometrage';
 import {Intervention} from '../containers/Intervention';
 import {Vehicles} from '../containers/Vehicles';
 import {Setting} from '../containers/Setting';
-import SignUp from '../containers/Auth/SignUp/SignUp';
 import {History} from '../containers/History';
 import {PDFViewer} from '../containers/PDFViewer';
 import {Promotions} from '../containers/Promotion';
 import AddCar from '../containers/Vehicles/AddCar/AddCar';
-import {AsyncStorageUtils} from '../utils/asyncStorageUtils';
-import {AuthContext} from '../providers/AuthProvider';
 
 const {Navigator, Screen} = createDrawerNavigator();
 
 const HomeNavigation = () => {
-  const {token, setToken} = useContext(AuthContext);
-
-  useEffect(() => {
-    AsyncStorageUtils.getToken().then(res => {
-      setToken(res);
-    });
-  }, []);
-
   return (
     <Navigator
       screenOptions={{
@@ -36,27 +23,17 @@ const HomeNavigation = () => {
           borderBottomRightRadius: 12,
         },
       }}
-      initialRouteName="SignIn"
+      initialRouteName="Home"
       drawerContent={props => <Sidemenu {...props} />}>
-      {token ? (
-        <>
-          <Screen name="Home" component={Home} />
-          <Screen name="Kilometrage" component={Kilometrage} />
-          <Screen name="Intervention" component={Intervention} />
-          <Screen name="Vehicles" component={Vehicles} />
-          <Screen name="Setting" component={Setting} />
-          <Screen name="PDFViewer" component={PDFViewer} />
-          <Screen name="History" component={History} />
-          <Screen name="Promotions" component={Promotions} />
-          <Screen name="AddCar" component={AddCar} />
-        </>
-      ) : (
-        <>
-          <Screen name="AuthNavigation" component={AuthNavigation} />
-          <Screen name="SignIn" component={SignIn} />
-          <Screen name="SignUp" component={SignUp} />
-        </>
-      )}
+      <Screen name="Home" component={Home} />
+      <Screen name="Kilometrage" component={Kilometrage} />
+      <Screen name="Intervention" component={Intervention} />
+      <Screen name="Vehicles" component={Vehicles} />
+      <Screen name="Setting" component={Setting} />
+      <Screen name="PDFViewer" component={PDFViewer} />
+      <Screen name="History" component={History} />
+      <Screen name="Promotions" component={Promotions} />
+      <Screen name="AddCar" component={AddCar} />
     </Navigator>
   );
 };
