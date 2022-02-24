7e602a7250e6ef7ff9b082c35b9d35c01efc1c0a
Zoniaina <fzoniaina@gmail.com>
2022-02-24 09:25:33 +0300

    update App container replace navcontainer to approute

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,4 +1,3 @@
-import {NavigationContainer} from '@react-navigation/native';
 import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
@@ -6,10 +5,9 @@ import * as eva from '@eva-design/eva';
 import {default as defaultTheme} from '../theme/default-theme.json';
 import {default as mapping} from '../../mapping.json';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
-import HomeNavigation from '../navigation/HomeNavigation';
 import {Provider} from 'react-redux';
 import {store} from '../store';
-import {AuthProvider} from '../providers/AuthProvider';
+import AppRoute from '../navigation/AppRoute';
 
 const App = () => {
   useEffect(() => {
@@ -23,11 +21,7 @@ const App = () => {
         {...eva}
         theme={{...eva.light, ...defaultTheme}}
         customMapping={mapping as any}>
-        <NavigationContainer>
-          <AuthProvider>
-            <HomeNavigation />
-          </AuthProvider>
-        </NavigationContainer>
+        <AppRoute />
       </ApplicationProvider>
     </Provider>
   );
