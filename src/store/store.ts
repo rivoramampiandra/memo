710f0a9a8de0f6a9c82dc658cb1c2830dc3bdf8f
Zoniaina <fzoniaina@gmail.com>
2022-02-24 09:26:35 +0300

    update redux store | setup

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,15 @@
 import {configureStore} from '@reduxjs/toolkit';
-import defaultReduer from './reducers/defaultSlice';
+import authReducer from './reducers/authSlice';
+import carReducer from './reducers/carSlice';
 
 const store = configureStore({
   reducer: {
-    default: defaultReduer,
+    auth: authReducer,
+    car: carReducer,
   },
 });
 
 export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
