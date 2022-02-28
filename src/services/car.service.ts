fcb293b95da90c16ccf99f69239097cdc75ad9bf
Zoniaina <fzoniaina@gmail.com>
2022-02-28 11:00:38 +0300

    carservice update

--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -1,13 +1,38 @@
+import {AsyncStorageUtils} from './../utils/asyncStorageUtils';
 import axiosClient, {axiosOCRClient} from '../lib/axios.lib';
+import {AuthService} from './auth.service';
 
 export class CarService {
-  static async ocrScanCard(data: {carteGrise: Blob}) {
+  static async ocrScanCard(data: any) {
     return await axiosOCRClient.post('analyseCarteGrise', data);
   }
 
-  static async addTemporaryCar(userId: string, data: any) {
+  static async getCar(carId: number) {
+    const userId = await AsyncStorageUtils.getUserID();
+    if (!userId) throw new Error('Utilisateur inconnu');
+    return await axiosClient.get(`user/${userId}/carEntity/${carId}`);
+  }
+
+  static async getCars() {
+    const userId = await AsyncStorageUtils.getUserID();
+    if (!userId) throw new Error('Utilisateur inconnu');
+    const {data} = await axiosClient.get(`user/${userId}/carEntitys`);
+    return data;
+  }
+
+  static async addTemporaryCar(userId: number, data: any) {
     return await axiosClient.post(`user/${userId}/addTemporaryCar`, data);
   }
 
-  static async softDeleteCar() {}
+  static async softDeleteCar(userId: number, carId: number) {
+    return await axiosClient.delete(
+      `user/carEntity/${userId}/${carId}/softDelete`,
+    );
+  }
+
+  static async updateMilleage(userId: number, carId: number, milleage: number) {
+    return await axiosClient.put(
+      `user/${userId}/carEntity/${carId}/mileage/${milleage}`,
+    );
+  }
 }
