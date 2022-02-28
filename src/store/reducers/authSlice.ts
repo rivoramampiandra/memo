dcf77e954d47f96f13f08fc690d0d318015d71cd
Zoniaina <fzoniaina@gmail.com>
2022-02-28 11:00:04 +0300

    update auth slice

--- src/store/reducers/authSlice.ts
+++ src/store/reducers/authSlice.ts
@@ -7,7 +7,9 @@ type authDataType = {
   userName?: string | null;
   tempToken?: string | null;
   tempPassword?: string | null;
-  id?: string | null;
+  id?: number | null;
+  firstName?: string | null;
+  lastName?: string | null;
 };
 
 const initialState: authDataType = {
@@ -17,37 +19,41 @@ const initialState: authDataType = {
   tempToken: null,
   tempPassword: null,
   id: null,
+  firstName: null,
+  lastName: null,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
+    setLogin: (state, action: PayloadAction<boolean>) => {
+      state.isLoggedIn = action.payload;
+    },
     setSignIn: (state, action: PayloadAction<authDataType>) => {
       state = {
         ...state,
         ...action.payload,
       };
+      return state;
     },
-    setToken: (state, action: PayloadAction<authDataType>) => {
-      state = {
-        ...state,
-        tempToken: action.payload.tempToken,
-      };
+    setToken: (state, action: PayloadAction<string>) => {
+      state['tempToken'] = action.payload;
     },
     setSignOut: state => {
-      state = {...state, ...initialState};
+      state.isLoggedIn = false;
     },
   },
 });
 
-export const {setSignIn, setSignOut, setToken} = authSlice.actions;
+export const {setSignIn, setLogin, setSignOut, setToken} = authSlice.actions;
 
 export const getLoginState = createSelector(
   (state: RootState) => state.auth.isLoggedIn,
   isLoggedIn => isLoggedIn,
 );
 
+export const getCurrentUser = (state: RootState) => state.auth;
 export const getTempToken = (state: RootState) => state.auth.tempToken;
 export const getTempPassword = (state: RootState) => state.auth.tempPassword;
 export const getUserId = (state: RootState) => state.auth.id;
