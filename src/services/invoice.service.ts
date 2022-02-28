cad759a5f16f7330c5c7a7e61ca72a789a0dc271
Zoniaina <fzoniaina@gmail.com>
2022-02-28 11:00:55 +0300

    invoice service update

--- src/services/invoice.service.ts
+++ src/services/invoice.service.ts
@@ -1,7 +1,22 @@
-import axiosClient from '../lib/axios.lib';
+import axiosClient, {axiosOCRClient} from '../lib/axios.lib';
 
 export class InvoiceService {
-  static async scanOCR() {}
+  static async scanIvoice(data: FormData) {
+    return await axiosOCRClient.post(`analyseInvoice`, data);
+  }
 
-  static async saveInvoice() {}
+  static async addInvoiceStep1(data: any) {
+    return await axiosOCRClient.post(
+      `invoice/addInvoiceStep1/user/$userId}`,
+      data,
+    );
+  }
+
+  static async addInvoiceStep2(userId: number, data: any) {
+    return await axiosOCRClient.post(`addInvoiceStep2/user/${userId}`, data);
+  }
+
+  static async manualProcessInfo(userId: string) {
+    return await axiosOCRClient.post(`manualProcessInfo/user/${userId}`);
+  }
 }
