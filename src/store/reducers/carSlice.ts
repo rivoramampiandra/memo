5a4d704ae6c38f5b018fdede529fccf5a259c6be
Zoniaina <fzoniaina@gmail.com>
2022-02-28 11:00:09 +0300

    update car slive

--- src/store/reducers/carSlice.ts
+++ src/store/reducers/carSlice.ts
@@ -1,19 +1,24 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 const initialState: any = {
-  id: null,
+  currentCar: {
+    id: null,
+  },
+  allCars: [],
 };
 
 const carSlice = createSlice({
   name: 'car',
   initialState,
   reducers: {
-    changeCar: (state: any, action: PayloadAction<any>) => {},
+    changeCar: (state: any, action: PayloadAction<number>) => {
+      state['currentCar'] = {id: action.payload};
+    },
   },
 });
 
 export const {changeCar} = carSlice.actions;
 
-export const currentCar = (state: any) => state.auth.isLoggedIn;
+export const getCurrentCar = (state: any) => state.car.currentCar;
 
 export default carSlice.reducer;
