37bc0c450a807f615d45c3a8d1c4272d9f7807da
Zoniaina <fzoniaina@gmail.com>
2022-02-28 10:59:35 +0300

    add interceptor for auth: axios

--- src/lib/axios.lib.ts
+++ src/lib/axios.lib.ts
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios, {AxiosRequestConfig} from 'axios';
+import {AsyncStorageUtils} from '../utils/asyncStorageUtils';
 
 const BASE_URL = 'https://app.memoracar.com';
 const PORT = 16000;
@@ -15,14 +16,27 @@ const axiosClient = axios.create({
   },
 });
 
+axiosClient.interceptors.request.use(
+  async (config: AxiosRequestConfig) => {
+    const token = await AsyncStorageUtils.getToken();
+    if (token) {
+      config.headers!.Authorization = 'Bearer ' + token;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+export default axiosClient;
+
 export const axiosOCRClient = axios.create({
   baseURL: `${OCR_BASE_URL}:${OCR_PORT}/`,
   timeout: 3000,
   headers: {
-    'Content-Type': 'application/json',
     Accept: 'application/json',
     Authorization: AUTHKEY,
+    'Content-type': 'multipart/form-data',
   },
 });
-
-export default axiosClient;
