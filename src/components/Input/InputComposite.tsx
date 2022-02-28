41bce0408a1ccb694d6c1186320cbbea5c1e26fa
Zoniaina <fzoniaina@gmail.com>
2022-02-28 10:56:45 +0300

    fix typo: inputcomposite

--- src/components/Input/InputComposite.tsx
+++ src/components/Input/InputComposite.tsx
@@ -7,7 +7,10 @@ const InputComposite = (props: TextInputProps) => {
   return (
     <View style={[styles.input, styles.composite]}>
       <Icon name="search-outline" fill="#8E8F92" style={styles.compositeIcon} />
-      <TextInput {...props} style={{flex: 1}} />
+      <TextInput
+        {...props}
+        style={{flex: 1, fontFamily: 'Montserrat-Regular', fontSize: 12}}
+      />
     </View>
   );
 };
