84af182d02e4780a0095834b4ae3d7b322fe0717
Zoniaina <fzoniaina@gmail.com>
2022-03-07 18:42:31 +0300

    header nav update

--- src/components/Layout/Header/HeaderNav.tsx
+++ src/components/Layout/Header/HeaderNav.tsx
@@ -4,42 +4,22 @@ import {Icon, Text} from '@ui-kitten/components';
 import React, {useEffect, useState} from 'react';
 import {View, Image, TouchableOpacity} from 'react-native';
 import image from '../../../constant/image';
-import {CarService} from '../../../services/car.service';
-import {AsyncStorageUtils} from '../../../utils/asyncStorageUtils';
+import {useAppSelector} from '../../../store/hooks';
+import {getCurrentCar} from '../../../store/reducers/carSlice';
 import {styles} from './Header.style';
 
 const HeaderNav = () => {
   const isDrawerOpen = useDrawerStatus() === 'open';
   const navigation = useNavigation();
+  const currentCar = useAppSelector(getCurrentCar);
 
   const handleOpenDrawer = () => {
     navigation.dispatch(DrawerActions.toggleDrawer());
   };
 
-  const getCurrentCar = async () => {
-    return await AsyncStorageUtils.getCurrentCarId();
-  };
+  const getCarInfo = async () => {};
 
-  const getCarInfo = async () => {
-    const carId = await getCurrentCar();
-    console.log(
-      '🚀 ~ file: HeaderNav.tsx ~ line 25 ~ getCarInfo ~ carId',
-      carId,
-    );
-    if (!carId) throw new Error('Car introuvable');
-    return await CarService.getCar(Number(carId));
-  };
-
-  useEffect(() => {
-    getCurrentCar().then(() => {
-      getCarInfo().then(res => {
-        console.log(
-          '🚀 ~ file: HeaderNav.tsx ~ line 32 ~ getCarInfo ~ res',
-          res,
-        );
-      });
-    });
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <View style={styles.navContainer}>
@@ -50,7 +30,14 @@ const HeaderNav = () => {
           style={{width: 32, height: 32}}
         />
       </TouchableOpacity>
-      <Text category="h1">Ma peugeot 206</Text>
+      {currentCar && currentCar.id && (
+        <Text category="h1">
+          Ma{' '}
+          {currentCar.model && currentCar.brand
+            ? `${currentCar.brand} ${currentCar.model}`
+            : `car ${currentCar.id}`}
+        </Text>
+      )}
       <TouchableOpacity
         onPress={() => navigation.navigate('Intervention' as any)}>
         <Image
