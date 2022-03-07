1b4be977b58ed786a62132328b028589af7807cb
Zoniaina <fzoniaina@gmail.com>
2022-03-07 18:45:02 +0300

    update car type wip

--- src/types/car.ts
+++ src/types/car.ts
@@ -1,1 +1,33 @@
 export interface IAddCarInfo {}
+
+export interface ICarModel {
+  brand: string;
+  carModelId: number;
+  energy: string;
+  finition: string;
+  fiscalPower: number;
+  horsePower: number;
+  kwPower: number;
+  maintenancePlan: any;
+  model: string;
+  motor: string;
+  transmissionType: string;
+  typeRearBrake: string | null;
+}
+
+export interface ICar {
+  averageMileagePerYear: 150000;
+  carEntityId: 62;
+  carModel: ICarModel | null;
+  deleted: false;
+  hasClim: true;
+  immatriculationDate: string;
+  lastDateMileage: string;
+  lastMileageValidated: number;
+  licencePlate: string;
+  mileage: string;
+  mileagePerDayEstimated: number;
+  mileageWhenSubscribed: number;
+  userDB: string | null;
+  vin: string;
+}
