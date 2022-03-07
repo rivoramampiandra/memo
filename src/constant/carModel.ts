cd63d0cc94ad2465ab89b1d39c653eca515b956d
Zoniaina <fzoniaina@gmail.com>
2022-03-07 18:42:43 +0300

    const car model

---
+++ src/constant/carModel.ts
@@ -0,0 +1,9 @@
+import image from './image';
+
+export const CarModel = {};
+
+export const carBrand: any = {
+  Peugeot: image.PEUGEOT,
+  Audi: image.AUDI,
+  Mercedes: image.MERCEDES,
+};
---
+++ src/constant/intervetionType.ts
@@ -0,0 +1,746 @@
+import image from './image';
+
+export const InterventionTypes = {
+  engdra1: {
+    description: 'huile moteur',
+    image: image.LIQUID_COOLER,
+  },
+
+  engdra2: {
+    description: 'niveau huile moteur',
+    image: image.LIQUID_COOLER,
+  },
+  engfil1: {
+    description: 'filtre  hydraulique de directionr',
+    image: image.LIQUID_COOLER,
+  },
+  engfil2: {
+    description: 'soupape de filtre a  carburant',
+    image: image.LIQUID_COOLER,
+  },
+  engfil3: {
+    description: 'filtre a huile',
+    image: image.LIQUID_COOLER,
+  },
+  engfil4: {
+    description: 'filtre a air',
+    image: image.LIQUID_COOLER,
+  },
+  engfil5: {
+    description: ' filtre a carburant',
+    image: image.LIQUID_COOLER,
+  },
+  engfil6: {
+    description: 'filtre coupleur de transmission',
+    image: image.LIQUID_COOLER,
+  },
+  engfil7: {
+    description: ' filtre a habitacle',
+    image: image.LIQUID_COOLER,
+  },
+  engclu1: {
+    description: "kit d'embrayager",
+    image: image.LIQUID_COOLER,
+  },
+  engclu2: {
+    description: 'volant moteur',
+    image: image.LIQUID_COOLER,
+  },
+  engclu3: {
+    description: "disque d'embrayage",
+    image: image.LIQUID_COOLER,
+  },
+  engclu4: {
+    description: "butee d'embrayager",
+    image: image.LIQUID_COOLER,
+  },
+  engclu5: {
+    description: " fourchette d'embrayage",
+    image: image.LIQUID_COOLER,
+  },
+  engclu6: {
+    description: "emetteur d'embrayage",
+    image: image.LIQUID_COOLER,
+  },
+  engclu7: {
+    description: "recepteur d'embrayage",
+    image: image.LIQUID_COOLER,
+  },
+  engclu8: {
+    description: "	cable d'embrayage",
+    image: image.LIQUID_COOLER,
+  },
+  engclu9: {
+    description: "roulement de pallier d'embrayage",
+    image: image.LIQUID_COOLER,
+  },
+  enggea1: {
+    description: '	support de boite de vitesse',
+    image: image.LIQUID_COOLER,
+  },
+  enggea2: {
+    description: 'valve de commande de transmission automatique',
+    image: image.LIQUID_COOLER,
+  },
+  enggea3: {
+    description: 'capteur de vitesse',
+    image: image.LIQUID_COOLER,
+  },
+  enggea4: {
+    description: "radiateur d'huile de  boite de vitesse automatique",
+    image: image.LIQUID_COOLER,
+  },
+  enggea5: {
+    description: ' composant boite de vitesse',
+    image: image.LIQUID_COOLER,
+  },
+  enggea6: {
+    description: 'tirette a cable , trianglerie de boite',
+    image: image.LIQUID_COOLER,
+  },
+  enggea7: {
+    description: '	filtre coupleur de transmission',
+    image: image.LIQUID_COOLER,
+  },
+  enggea8: {
+    description: '	huile boite de vitesse',
+    image: image.LIQUID_COOLER,
+  },
+  engthe1: {
+    description: 'liquide de refroidissement',
+    image: image.LIQUID_COOLER,
+  },
+  engthe2: {
+    description: 'niveau liquide de refroidissementr',
+    image: image.LIQUID_COOLER,
+  },
+  engthe3: {
+    description: ' radiateur liquide de refroidissement',
+    image: image.LIQUID_COOLER,
+  },
+  engthe4: {
+    description: 'durite de refroidissement',
+    image: image.LIQUID_COOLER,
+  },
+  engthe5: {
+    description: 'thermostat',
+    image: image.LIQUID_COOLER,
+  },
+  engthe6: {
+    description: 'sonde de temperature de liquide de refroidissement',
+    image: image.LIQUID_COOLER,
+  },
+  engthe7: {
+    description: "sonde de temperature d'huile",
+    image: image.LIQUID_COOLER,
+  },
+  engthe8: {
+    description: 'pompe a eau',
+    image: image.LIQUID_COOLER,
+  },
+  engthe9: {
+    description: "deflecteur d'air du ventilateur",
+    image: image.LIQUID_COOLER,
+  },
+  engthe10: {
+    description: ' ventilateur moteur',
+    image: image.LIQUID_COOLER,
+  },
+  engthe11: {
+    description: 'commande de ventillation de chauffage',
+    image: image.LIQUID_COOLER,
+  },
+  engthe12: {
+    description: 'intercooler',
+    image: image.LIQUID_COOLER,
+  },
+  engthe13: {
+    description: "radiateur d'huile",
+    image: image.LIQUID_COOLER,
+  },
+  engthe14: {
+    description: 'capteur de niveau de lave glace',
+    image: image.LIQUID_COOLER,
+  },
+  engair1: {
+    description: 'compresseur de climatisation',
+    image: image.LIQUID_COOLER,
+  },
+  engair2: {
+    description: 'condenseur de climatisation',
+    image: image.LIQUID_COOLER,
+  },
+  engair3: {
+    description: 'appareil de commande de climatisation',
+    image: image.LIQUID_COOLER,
+  },
+  engair4: {
+    description: 'pressostat de climatisation',
+    image: image.LIQUID_COOLER,
+  },
+  engair5: {
+    description: 'conduite de climatisation',
+    image: image.LIQUID_COOLER,
+  },
+  engair6: {
+    description: " pulseur d'air",
+    image: image.LIQUID_COOLER,
+  },
+  engair7: {
+    description: 'capteur de temperature exterieur',
+    image: image.LIQUID_COOLER,
+  },
+  engair8: {
+    description: 'capteur de temperature interieur',
+    image: image.LIQUID_COOLER,
+  },
+  engair9: {
+    description: '	valve de reglage de compresseur',
+    image: image.LIQUID_COOLER,
+  },
+  engair10: {
+    description: 'radiateur de chauffage',
+    image: image.LIQUID_COOLER,
+  },
+  engair11: {
+    description: 'gaz de climatisation',
+    image: image.LIQUID_COOLER,
+  },
+  engoth1: {
+    description: '	galet tendeur',
+    image: image.LIQUID_COOLER,
+  },
+  engoth2: {
+    description: 'poulie de renvoi',
+    image: image.LIQUID_COOLER,
+  },
+  engoth3: {
+    description: 'support moteur G et D',
+    image: image.LIQUID_COOLER,
+  },
+  engoth4: {
+    description: 'segmentation',
+    image: image.LIQUID_COOLER,
+  },
+  engoth5: {
+    description: "joint de carter d'huile",
+    image: image.LIQUID_COOLER,
+  },
+  engoth6: {
+    description: 'joint spi de vilebrequin',
+    image: image.LIQUID_COOLER,
+  },
+  engoth7: {
+    description: '	joints de queue de soupape',
+    image: image.LIQUID_COOLER,
+  },
+  engoth8: {
+    description: "soupape d'admission",
+    image: image.LIQUID_COOLER,
+  },
+  engoth9: {
+    description: "	soupape d'echappement",
+    image: image.LIQUID_COOLER,
+  },
+  engoth10: {
+    description: "	boitier papillon d'admission",
+    image: image.LIQUID_COOLER,
+  },
+  engoth11: {
+    description: "capteur de debimetre d'air",
+    image: image.LIQUID_COOLER,
+  },
+  engoth12: {
+    description: 'turbo',
+    image: image.LIQUID_COOLER,
+  },
+  engoth13: {
+    description: 'gaine de suralimentation',
+    image: image.LIQUID_COOLER,
+  },
+  engoth14: {
+    description: 'capteur de pression de suralimentation',
+    image: image.LIQUID_COOLER,
+  },
+  engoth15: {
+    description: 'capteur de position de pedale',
+    image: image.LIQUID_COOLER,
+  },
+  engoth16: {
+    description: 'capteur de position du papillon',
+    image: image.LIQUID_COOLER,
+  },
+  engoth17: {
+    description: '	vanne EGR',
+    image: image.LIQUID_COOLER,
+  },
+  engoth18: {
+    description: "collecteur d'admission",
+    image: image.LIQUID_COOLER,
+  },
+  engoth19: {
+    description: 'pompe a huile moteur',
+    image: image.LIQUID_COOLER,
+  },
+  engoth20: {
+    description: "collecteur d'echappement",
+    image: image.LIQUID_COOLER,
+  },
+  engoth21: {
+    description: "	sonde de pression d'huile",
+    image: image.LIQUID_COOLER,
+  },
+  engoth22: {
+    description: "	cable d'accelerateur",
+    image: image.LIQUID_COOLER,
+  },
+  engoth23: {
+    description: "sonde d'huile moteur",
+    image: image.LIQUID_COOLER,
+  },
+  engoth24: {
+    description: 'capteur de cliquetis',
+    image: image.LIQUID_COOLER,
+  },
+  engoth25: {
+    description: "conduite d'huile rigide",
+    image: image.LIQUID_COOLER,
+  },
+  engoth26: {
+    description: "capteur position d'abre a came",
+    image: image.LIQUID_COOLER,
+  },
+  engoth27: {
+    description: 'capteur PMH',
+    image: image.LIQUID_COOLER,
+  },
+  engoth28: {
+    description: 'appareil de commande, gestion moteur (calculateur)',
+    image: image.LIQUID_COOLER,
+  },
+  engoth29: {
+    description: 'poulie damper',
+    image: image.LIQUID_COOLER,
+  },
+  engoth30: {
+    description: 'pompe a injection',
+    image: image.LIQUID_COOLER,
+  },
+  engoth31: {
+    description: 'injecteur',
+    image: image.LIQUID_COOLER,
+  },
+  engoth32: {
+    description: 'comun rail , rampe de carburant',
+    image: image.LIQUID_COOLER,
+  },
+  engoth33: {
+    description: 'capteur de regulation de pression carburant',
+    image: image.LIQUID_COOLER,
+  },
+  engoth34: {
+    description: 'capteur de niveau carburant',
+    image: image.LIQUID_COOLER,
+  },
+  engoth35: {
+    description: 'tuyan et conduite de carburant',
+    image: image.LIQUID_COOLER,
+  },
+  engoth36: {
+    description: "bobine d'allumage",
+    image: image.LIQUID_COOLER,
+  },
+  engoth37: {
+    description: "arbre d'equilibrage",
+    image: image.LIQUID_COOLER,
+  },
+  engoth38: {
+    description: "faiceau d'allumage",
+    image: image.LIQUID_COOLER,
+  },
+  engoth39: {
+    description: 'courroie de distribution',
+    image: image.LIQUID_COOLER,
+  },
+  engoth40: {
+    description: 'courroie accessoire',
+    image: image.LIQUID_COOLER,
+  },
+  engoth41: {
+    description: "bougies d'allumager",
+    image: image.LIQUID_COOLER,
+  },
+  engoth42: {
+    description: '	bougies de prechauffage',
+    image: image.LIQUID_COOLER,
+  },
+  engoth43: {
+    description:
+      "	niveaux  d'huile, de liquide de refroidissement et de liquide de frein ",
+    image: image.LIQUID_COOLER,
+  },
+  chasho1: {
+    description: 'coupelle amortiseur',
+    image: image.LIQUID_COOLER,
+  },
+  chasho2: {
+    description: ' ressort amortisseur',
+    image: image.LIQUID_COOLER,
+  },
+  chasho3: {
+    description: 'amortisseur',
+    image: image.LIQUID_COOLER,
+  },
+  chasho4: {
+    description: " butee d'amortisseur",
+    image: image.LIQUID_COOLER,
+  },
+  chaoth1: {
+    description: 'barre stabilisatrice',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth2: {
+    description: 'silent bloc barre stabilisatrice',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth3: {
+    description: ' biellette de barre stabilisatrice',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth4: {
+    description: 'rotule de suspension',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth5: {
+    description: 'bras de suspenssion',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth6: {
+    description: 'silent bloc de triangle',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth7: {
+    description: 'biellette de direction',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth8: {
+    description: 'rotule axiale',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth9: {
+    description: 'rrotule de direction',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth10: {
+    description: 'cremaillere de direction',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth11: {
+    description: 'souflet de direction',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth12: {
+    description: 'pompe de direction assiste',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth13: {
+    description: "detecteur de l'angle de braquage",
+    image: image.LIQUID_COOLER,
+  },
+  chaoth14: {
+    description: ' roulement de roue',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth15: {
+    description: 'cardan Gauche',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth16: {
+    description: 'cardan Droit',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth17: {
+    description: 'joint homonocinetique',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth18: {
+    description: 'tripode de cardan',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth19: {
+    description: 'soufflet de cardanr',
+    image: image.LIQUID_COOLER,
+  },
+  chaoth20: {
+    description: "palier d'arbre moteur",
+    image: image.LIQUID_COOLER,
+  },
+  elebat1: {
+    description: '	batterie',
+    image: image.LIQUID_COOLER,
+  },
+  eleoth1: {
+    description: 'tresse de masse',
+    image: image.LIQUID_COOLER,
+  },
+  elealt1: {
+    description: 'falternateur',
+    image: image.LIQUID_COOLER,
+  },
+  elealt2: {
+    description: "poulie d'alternateur",
+    image: image.LIQUID_COOLER,
+  },
+  elealt3: {
+    description: "regulateur d'alternateur",
+    image: image.LIQUID_COOLER,
+  },
+  elesta1: {
+    description: 'embrayage de roue libre demarreur',
+    image: image.LIQUID_COOLER,
+  },
+  elesta2: {
+    description: 'solenoïde de demarreur',
+    image: image.LIQUID_COOLER,
+  },
+  braoth1: {
+    description: 'flexible de frein',
+    image: image.LIQUID_COOLER,
+  },
+  braoth2: {
+    description: 'etrier de frein',
+    image: image.LIQUID_COOLER,
+  },
+  braoth3: {
+    description: 'maitre cylindre',
+    image: image.LIQUID_COOLER,
+  },
+  braoth4: {
+    description: ' contacteur de feux stop',
+    image: image.LIQUID_COOLER,
+  },
+  braoth5: {
+    description: 'mastervac',
+    image: image.LIQUID_COOLER,
+  },
+  braoth6: {
+    description: '	conduite de frein rigide',
+    image: image.LIQUID_COOLER,
+  },
+  braoth7: {
+    description: 'cable de frein a main',
+    image: image.LIQUID_COOLER,
+  },
+  braoth8: {
+    description: "piston d'etrier de frein",
+    image: image.LIQUID_COOLER,
+  },
+  braoth9: {
+    description: "temoin d'usure plaquette de frein",
+    image: image.LIQUID_COOLER,
+  },
+  braoth10: {
+    description: 'capteur et bague ABS',
+    image: image.LIQUID_COOLER,
+  },
+  braoth11: {
+    description: 'vase a expansion liquide de frein',
+    image: image.LIQUID_COOLER,
+  },
+  braoth12: {
+    description: 'groupe hydraulique (repartiteur hydraulique)',
+    image: image.LIQUID_COOLER,
+  },
+  braoth13: {
+    description: 'pompe a vide de freinage',
+    image: image.LIQUID_COOLER,
+  },
+  braoth14: {
+    description: 'liquide de frein',
+    image: image.LIQUID_COOLER,
+  },
+  braoth15: {
+    description: 'niveau liquide de frein',
+    image: image.LIQUID_COOLER,
+  },
+  brapad1: {
+    description: '	Plaquette de frein AV',
+    image: image.LIQUID_COOLER,
+  },
+  brapad2: {
+    description: 'machoires de frein',
+    image: image.LIQUID_COOLER,
+  },
+  brapad3: {
+    description: 'Plaquette de frein AR',
+    image: image.LIQUID_COOLER,
+  },
+  bradis1: {
+    description: 'tambour de frein',
+    image: image.LIQUID_COOLER,
+  },
+  bradis2: {
+    description: 'disque de frein AV',
+    image: image.LIQUID_COOLER,
+  },
+  bradis3: {
+    description: 'disque de frein AR',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod1: {
+    description: 'optiques de phare AVG',
+    image: image.LIQUID_COOLER,
+  },
+  // bodbod1: {
+  //   description: 'optiques de phare AVD',
+  //   image: image.LIQUID_COOLER,
+  // },
+  // bodbod1: {
+  //   description: 'optiques de phare ARG',
+  //   image: image.LIQUID_COOLER,
+  // },
+  // bodbod1: {
+  //   description: 'optiques de phare ARD',
+  //   image: image.LIQUID_COOLER,
+  // },
+  // bodbod1: {
+  //   description: 'anti-brouilared AV',
+  //   image: image.LIQUID_COOLER,
+  // },
+  // bodbod1: {
+  //   description: 'anti-brouilared AR',
+  //   image: image.LIQUID_COOLER,
+  // },
+  // bodbod1: {
+  //   description: 'feux diurne',
+  //   image: image.LIQUID_COOLER,
+  // },
+  bodbod2: {
+    description: 'retroviseur D',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod3: {
+    description: 'retroviseur G',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod4: {
+    description: 'pare-choc AV',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod5: {
+    description: '	pare-choc AR',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod6: {
+    description: 'des capteurs de recul',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod7: {
+    description: 'des pare-boue',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod8: {
+    description: 'serrure de porte AVD',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod9: {
+    description: 'serrure de porte AVG',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod10: {
+    description: '	serrure de porte ARD',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod11: {
+    description: 'serrure de porte ARG',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod12: {
+    description: 'serrure de coffre',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod13: {
+    description: ' verin de capot',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod14: {
+    description: 'verin de hayon',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod15: {
+    description: 'leve-vitre',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod16: {
+    description: 'poignee de porte',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod17: {
+    description: 'essuis glace',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod18: {
+    description: 'pompe a lave glace',
+    image: image.LIQUID_COOLER,
+  },
+  bodbod19: {
+    description: "moteur d'essuis-glage",
+    image: image.LIQUID_COOLER,
+  },
+  tirtir1: {
+    description: 'pneus Avant',
+    image: image.BRAKE,
+  },
+  tirtir2: {
+    description: '	pneus Arrierer',
+    image: image.LIQUID_COOLER,
+  },
+  tirtir3: {
+    description: 'pression des pneus',
+    image: image.LIQUID_COOLER,
+  },
+  exhexh1: {
+    description: "pot d'echappement complet",
+    image: image.LIQUID_COOLER,
+  },
+  exhexh2: {
+    description: '	Silencieux intermediaire echappement ',
+    image: image.LIQUID_COOLER,
+  },
+  exhexh3: {
+    description: 'silencieux avant',
+    image: image.LIQUID_COOLER,
+  },
+  exhexh4: {
+    description: 'catalyseur',
+    image: image.LIQUID_COOLER,
+  },
+  exhexh5: {
+    description: 'filtre a particule',
+    image: image.LIQUID_COOLER,
+  },
+  exhexh6: {
+    description: "joint de collecteur d'echappement",
+    image: image.LIQUID_COOLER,
+  },
+  exhexh7: {
+    description: "silentbloc d'echappement",
+    image: image.LIQUID_COOLER,
+  },
+  exhexh8: {
+    description: 'protections thermique',
+    image: image.LIQUID_COOLER,
+  },
+  exhexh9: {
+    description: "	collecteur d'echappement",
+    image: image.LIQUID_COOLER,
+  },
+  tectec1: {
+    description: 'Controle technique periodique',
+    image: image.LIQUID_COOLER,
+  },
+  // enggea7: {
+  //   description: 'Coupleur arbre de transmission',
+  //   image: image.LIQUID_COOLER,
+  // },
+};
